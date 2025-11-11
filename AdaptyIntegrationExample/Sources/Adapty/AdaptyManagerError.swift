97243790c2383c62a6940ededc01b702cd74fd49
Sergei Fabian <me.sergeifabian@gmail.com>
2025-11-11 13:03:00 +0300

    Updates error handling for Adapty Manager

--- AdaptyIntegrationExample/Sources/Adapty/AdaptyManagerError.swift
+++ AdaptyIntegrationExample/Sources/Adapty/AdaptyManagerError.swift
@@ -1,4 +1,15 @@
-enum AdaptyManagerError: Error {
+import Foundation
+
+enum AdaptyManagerError: LocalizedError {
   case configurationFailed(Error)
   case paywallLoadingFailed(Error)
+
+  var errorDescription: String? {
+    switch self {
+    case .configurationFailed(let error):
+      return error.localizedDescription
+    case .paywallLoadingFailed(let error):
+      return error.localizedDescription
+    }
+  }
 }
